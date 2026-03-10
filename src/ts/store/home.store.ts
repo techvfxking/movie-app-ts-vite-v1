650cfb3c76324b702d85dde3ff0e3ca2d7e5802e
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2026-03-11 00:17:28 +0530

    BS-11-03-2026-Message: 1. Modified the home.store.ts based on the THomeStore type

--- src/ts/store/home.store.ts
+++ src/ts/store/home.store.ts
@@ -1,5 +1,13 @@
-const HomeStore = {
-    homeInputValue: ""
+import type { THomeStore } from "../types/home.component.type";
+
+const HomeStore: THomeStore = {
+    searchInputValue: "",
+    searchInputEvents: ["input","keypress"],
+    searchInput: null,
+    searchBtn: null,
+    clearBtn: null,
+    searchAlert: null,
+    searchLoader: null
 };
 
 export default HomeStore;
